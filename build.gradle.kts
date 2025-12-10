e41b8960ae85ee2d289ba8d8aa3d9392eb4a1534
George <bloogefest@gmail.com>
2025-12-10 21:40:58 +0300

    Изменение репозитория

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -14,6 +14,12 @@ java {
 }
 
 repositories {
+    maven("https://nexus.sogeor.com/repository/maven-public/") {
+        credentials {
+            username = findProperty("SOGEOR_NEXUS_USERNAME")?.toString() ?: System.getenv("NEXUS_USERNAME")
+            password = findProperty("SOGEOR_NEXUS_PASSWORD")?.toString() ?: System.getenv("NEXUS_PASSWORD")
+        }
+    }
     mavenCentral()
 }
 
@@ -26,33 +32,34 @@ configurations {
 extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
-    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
 
     compileOnly("org.jetbrains:annotations:${findProperty("org.jetbrains.annotations.version")}")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation(
-        "org.springdoc:springdoc-openapi-starter-webmvc-ui:${
-            findProperty("org.springdoc.springdoc-openapi-starter-webmvc-ui.version")
-        }"
-    )
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${
+        findProperty("org.springdoc.springdoc-openapi-starter-webmvc-ui.version")
+    }")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor(
-        "org.projectlombok:lombok-mapstruct-binding:${findProperty("org.projectlombok.lombok-mapstruct-binding")}"
-    )
+        "org.projectlombok:lombok-mapstruct-binding:${findProperty("org.projectlombok.lombok-mapstruct-binding")}")
 
-    runtimeOnly("org.postgresql:postgresql")
     implementation("com.zaxxer:HikariCP:${findProperty("com.zaxxer.HikariCP")}")
     implementation("org.mapstruct:mapstruct:${findProperty("org.mapstruct.mapstruct")}")
     annotationProcessor("org.mapstruct:mapstruct-processor:${findProperty("org.mapstruct.mapstruct")}")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
